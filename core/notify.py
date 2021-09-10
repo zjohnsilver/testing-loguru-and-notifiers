37912a15499492188be2ff125f48eab7fca94361
john.silver <johncarlosilver@gmail.com>
2021-09-09 21:42:01 -0300

    update core/notify

--- core/notify.py
+++ core/notify.py
@@ -1,17 +1,9 @@
 from notifiers.logging import NotificationHandler
+from .env import TELEGRAM_TOKEN, TELEGRAM_CHANNEL_ID
 
 defaults = {
-    "token": "1702989921:AAFk13Lpzupo9Hls4D4x3F6NFWovszfsK3g",
-    "chat_id": "-1001270811373",
+    "token": TELEGRAM_TOKEN,
+    "chat_id": TELEGRAM_CHANNEL_ID,
 }
 
 telegram_handler = NotificationHandler("telegram", defaults=defaults)
-
-
-gmail_params = {
-    "username": "johncarlosilver@gmail.com",
-    "password": "EcHDb^5uV*rv@e3oCvPUg",
-    "to": "johncarlosilver@gmail.com",
-}
-
-gmail_handler = NotificationHandler("gmail", defaults=gmail_params)

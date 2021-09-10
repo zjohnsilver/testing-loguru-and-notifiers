a793cafc1d9061f47cc16f43e50656ad60f56885
john.silver <johncarlosilver@gmail.com>
2021-09-09 21:42:52 -0300

    add function create_folders_if_not_exists in lib path

--- libs/path.py
+++ libs/path.py
@@ -10,3 +10,11 @@ def get_folders_names_from_path(path="."):
     ]
 
     return root_folders_names
+
+def create_folders_if_not_exists(folder_names = [], path=""):
+    for folder_name in folder_names:
+        folder_name = folder_name.split("/")[1]
+        folder_path = os.path.join(path, folder_name)
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)  
